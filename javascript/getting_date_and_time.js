cbf112a572aea4badd16126b9aa3c9e4550b8575
Gaurav Tewari <54894783+tewarig@users.noreply.github.com>
2020-04-19 12:21:22 +0530

    added few more topics

---
+++ javascript/getting_date_and_time.js
@@ -0,0 +1,13 @@
+sometimes you need to render date and time or even year in footer, of a site, you can do it very easily by using js.
+
+
+let time = new Date().toLocaleTimeString();
+console.log(time);
+
+
+for getting year
+var day = new Date();
+var year = day.getFullYear();
+
+
+
